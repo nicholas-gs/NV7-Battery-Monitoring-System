bc156fb2727cf70bad9845ed2230db5be79d66f2
Nicholas Gan <nicholasganshyan@gmail.com>
2019-12-24 22:40:55 +0800

    change mock transmitter code

--- Arduino/mock_transmitter/mock_transmitter.ino
+++ Arduino/mock_transmitter/mock_transmitter.ino
@@ -6,11 +6,35 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("99999");
-  delay(100);
-  Serial.println("11111");
-  delay(100);
-  Serial.println("22222");
-  delay(100);
-  Serial.println("33333");
+  Serial.println("00010");
+  delay(50);
+  Serial.println("00111");
+  delay(50);
+  Serial.println("00212");
+  delay(50);
+  Serial.println("00313");
+  delay(50);
+  Serial.println("100233");
+  delay(50);
+  Serial.println("10123");
+  delay(50);
+  Serial.println("10223");
+  delay(50);
+  Serial.println("10323");
+  delay(50);
+  Serial.println("20023");
+  delay(50);
+  Serial.println("20123");
+  delay(50);
+  Serial.println("20223");
+  delay(50);
+  Serial.println("20323");
+  delay(50);
+  Serial.println("30023");
+  delay(50);
+  Serial.println("30123");
+  delay(50);
+  Serial.println("30223");
+  delay(50);
+  Serial.println("30323");
 }
