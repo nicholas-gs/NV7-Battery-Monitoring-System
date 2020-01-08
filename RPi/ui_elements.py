07d938b64ffa5552e1a34b2f99872ae895f55d85
Nicholas Gan <nicholasganshyan@gmail.com>
2020-01-09 00:10:10 +0800

    add overheat indicator

--- RPi/ui_elements.py
+++ RPi/ui_elements.py
@@ -1,46 +1,45 @@
-# 2D array to keep track if a temperature sensor is working -- change value to 'False' if not working
-# If True --> Temperature sensor is working
+# 2D array to keep track of temperature offsets for sensors
 # Arranged in the 2D array according to [moduleID][SensorID]
 
 # IMPORTANT:
 
-# Modules are numbered -- 0,1,2,4,5,6. But can't tell which module ID corresponds to which physical module
+# Modules are numbered -- 0,1,2,4,5,6. But can't tell which module ID corresponds to which physical module!!!
 
 
-class ValidTemps:
+class TempSensors:
 
-    validSensors = [[True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True],  # Module 0
+    sensorOffsets = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module 0
 
-                    [True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True],  # Module 1
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module 1
 
-                    [True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True],  # Module 2
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module 2
 
-                    [True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True],  # Module NULL
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module NULL
 
-                    [True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True],  # Module 4
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module 4
 
-                    [True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True],  # Module 5
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module 5
 
-                    [True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True, True, True, True, True, True, True, True, True,
-                     True, True]  # Module 6
-                    ]
+                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                      0, 0],  # Module 6
+                     ]
--- code_gen.py
+++ code_gen.py
@@ -1,11 +1,16 @@
 i = 0
 while(i < 32):
-    print(""" if(tempData[""" + str(i) + """] != 0):
+    print("""if(tempData[""" + str(i) + """] != -1 and tempData[""" + str(i) + """] <= 50):
     self.mod5_""" + str(i) + """.display(tempData[""" + str(i) + """])
     palette = self.mod5_""" + str(i) + """.palette()      
     palette.setColor(palette.Background, Ui_MainWindow.greenColor)
     self.mod5_""" + str(i) + """.setPalette(palette)
-else:
+elif(tempData[""" + str(i) + """] != -1 and tempData[""" + str(i) + """] > 50 ):
+    self.mod5_""" + str(i) + """.display(tempData[""" + str(i) + """])
+    palette = self.mod5_""" + str(i) + """.palette()      
+    palette.setColor(palette.Background, Ui_MainWindow.redColor)
+    self.mod5_""" + str(i) + """.setPalette(palette)
+else:   
     palette = self.mod5_""" + str(i) + """.palette()      
     palette.setColor(palette.Background, Ui_MainWindow.yellowColor)
     self.mod5_""" + str(i) + """.setPalette(palette)
